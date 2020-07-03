bf6ddb8756ffe4684321475a7ce11899b84b715d
Sidney Douw <s.douw@gmx.de>
2020-07-03 17:28:41 +0100

    setup ts-jest

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/** @typedef {import('ts-jest')} */
+/** @type {import('@jest/types').Config.InitialOptions} */
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    globals: {
+        'ts-jest': {},
+    },
+}
