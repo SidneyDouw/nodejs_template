d930cc4905a762cc5ac54432c945bcfe254f5478
Sidney Douw <s.douw@gmx.de>
2020-08-03 16:22:42 +0100

    exluding '*.test.ts' pattern from compiling

--- gulpconfig/tasks/gulp_tsServer.js
+++ gulpconfig/tasks/gulp_tsServer.js
@@ -3,8 +3,12 @@ const fs = require('fs')
 module.exports = function (gulp, plugins, config, done) {
     let tsConfig = JSON.parse(fs.readFileSync(process.cwd() + '/tsconfig.json', 'utf8'))
 
+    // exlude files with *.test.ts
+    let s = config.paths.src.tsServer
+    let excludeTestString = '!' + s.slice(0, s.indexOf('.ts')) + '.test.ts'
+
     return gulp
-        .src(config.paths.src.tsServer)
+        .src([config.paths.src.tsServer, excludeTestString])
         .pipe(plugins.tslint())
         .pipe(plugins.if(!config.isProduction, plugins.sourcemaps.init()))
         .pipe(
